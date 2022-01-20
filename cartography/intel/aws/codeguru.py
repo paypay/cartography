af8cd5829e168c42047e799510fa4bfae82b8799
t-maoshengye <t-maosheng.ye@paypay-corp.co.jp>
2022-01-20 20:51:21 +0900

    sync and cleanup codeguru data

--- cartography/intel/aws/codeguru.py
+++ cartography/intel/aws/codeguru.py
@@ -1,12 +1,13 @@
 import json
 import logging
+import pprint
 from typing import Any
 from typing import Dict
 from typing import List
+from urllib import response
 
 import boto3
 import neo4j
-from botocore.exceptions import ClientError
 
 from cartography.util import aws_handle_regions
 from cartography.util import run_cleanup_job
@@ -23,38 +24,226 @@ def get_repository_associations(boto3_session: boto3.session.Session, region: st
     associations: List[Any] = []
     for page in paginator.paginate():
         associations.extend(page.get('RepositoryAssociationSummaries', []))
-
     return associations
 
 @timeit
-def get_association_name(associations: List[str]) -> List[str]:
-    name = []
+def get_association_names(associations: List[str]) -> List[str]:
+    names: List[Any] = []
     for assoc in associations:
-        name.append(assoc['Name'])
+        names.append(assoc['Name'])
 
-    return name
+    return names
+
+def get_recommendation_feedbacks(boto3_session: boto3.session.Session,region: str,code_reviews: List[str]) -> List[str]:
+    client = boto3_session.client('codeguru-reviewer', region_name=region)
+
+    queues: List[Dict] = []
+    for cr in code_reviews:
+        response = client.list_recommendation_feedback(CodeReviewArn=cr['CodeReviewArn'])
+        for queue in response['RecommendationFeedbackSummaries']:
+            queue['Reactions'] = queue['Reactions'][0]
+            queue['RecommendationId'] = queue['RecommendationId']
+            queues.append(queue)
+
+    return queues
 
 @timeit
 @aws_handle_regions
-def get_code_reviews(boto3_session: boto3.session.Session,region: str,associations: List[str]) -> List[str]:
+def get_code_reviews(boto3_session: boto3.session.Session,region: str,association_names: List[str]) -> List[str]:
     client = boto3_session.client('codeguru-reviewer', region_name=region)
-    response = client.list_code_reviews(States=['Completed'],Type='PullRequest',RepositoryNames=associations)
+    response = client.list_code_reviews(States=['Completed'],Type='PullRequest',RepositoryNames=association_names)
+    return response['CodeReviewSummaries']
 
-    return response
+@timeit
+@aws_handle_regions
+def get_recommendations(boto3_session: boto3.session.Session,region: str,code_reviews: List[str]) -> List[str]:
+    client = boto3_session.client('codeguru-reviewer', region_name=region)
+    recommendations: List[Any] = []
+    queues: List[Dict] = []
+    for cr in code_reviews:
+        response = client.list_recommendations(CodeReviewArn=cr['CodeReviewArn'])
+        for queue in response['RecommendationSummaries']:
+            queue['CodeReviewArn'] = cr['CodeReviewArn']
+            queues.append(queue)
 
-#def get_recommendations(boto3_session: boto3.session.Session,region: str,reviews: List[str]) -> List[str]:
+    return queues
+
+@timeit
+def load_repository_associations(
+    neo4j_session: neo4j.Session,
+    data: list,
+    region: str,
+    current_aws_account_id: str,
+    aws_update_tag: int,
+) -> None:
+    cypher = """
+    UNWIND {Repos} AS repo
+        MERGE (cga:CodeguruAssociation{associationarn: repo.AssociationArn})
+        ON CREATE SET cga.firstseen = timestamp()
+        SET  
+             cga.associationid = repo.AssociationId,
+             cga.name = repo.Name,
+             cga.owner = repo.Owner,
+             cga.providertype = repo.ProviderType,
+             cga.state = repo.State,
+             cga.lastupdated = {aws_update_tag}
+        WITH cga
+        MATCH (owner:AWSAccount{id: {AWS_ACCOUNT_ID}})
+        MERGE (owner)-[r:RESOURCE]->(cga)
+        ON CREATE SET cga.firstseen = timestamp()
+        SET cga.lastupdated = {aws_update_tag}
+    """
+
+    neo4j_session.run(
+        cypher,
+        Repos=data,
+        Region=region,
+        AWS_ACCOUNT_ID=current_aws_account_id,
+        aws_update_tag=aws_update_tag,
+    )
+
+@timeit
+def load_code_reviews(
+    neo4j_session: neo4j.Session,
+    data: list,
+    region: str,
+    current_aws_account_id: str,
+    aws_update_tag: int,
+) -> None:
+    cypher = """
+    UNWIND {Repos} AS repo
+        MERGE (cgc:CodeguruCodereview{codereviewarn: repo.CodeReviewArn})
+        ON CREATE SET cgc.firstseen = timestamp()
+        SET  
+             cgc.associationarn = repo.AssociationArn,
+             cgc.name = repo.Name,
+             cgc.owner = repo.Owner,
+             cgc.providertype = repo.ProviderType,
+             cgc.pullrequestId = repo.PullRequestId,
+             cgc.repositoryname = repo.RepositoryName,
+             cgc.state = repo.State,
+             cgc.type = repo.Type,
+             cgc.lastupdated = {aws_update_tag}
+        WITH cgc,repo
+        MATCH (cga:CodeguruAssociation{associationarn: repo.AssociationArn})
+        MERGE (cga)-[r:HAS_CODEREVIEW]->(cgc)
+        ON CREATE SET cgc.firstseen = timestamp()
+        SET cgc.lastupdated = {aws_update_tag}
+    """
+    queues: List[Dict] = []
+    for queue in data:
+        queue['AssociationArn'] = queue['CodeReviewArn'].split(':code-review:')[0]
+        queue['CodeReviewArn'] = queue['CodeReviewArn']
+        queues.append(queue)
+
+    neo4j_session.run(
+        cypher,
+        Repos=queues,
+        Region=region,
+        AWS_ACCOUNT_ID=current_aws_account_id,
+        aws_update_tag=aws_update_tag,
+    )
+
+@timeit
+def load_recommendations(
+    neo4j_session: neo4j.Session,
+    data: list,
+    region: str,
+    current_aws_account_id: str,
+    aws_update_tag: int,
+) -> None:
+    cypher = """
+    UNWIND {Repos} AS repo
+        MERGE (cgr:CodeguruRecommendation{recommendationid: repo.RecommendationId})
+        ON CREATE SET cgr.firstseen = timestamp()
+        SET  
+             cgr.codereviewarn = repo.CodeReviewArn,
+             cgr.description = repo.Description,
+             cgr.filepath = repo.FilePath,
+             cgr.recommendationcategory = repo.RecommendationCategory,
+             cgr.severity = repo.Severity,
+             cgr.recommendationid = repo.RecommendationId,
+             cgr.startline = repo.StartLine,
+             cgr.endLine = repo.EndLine,
+             cgr.lastupdated = {aws_update_tag}
+        WITH cgr,repo
+        MATCH (cgc:CodeguruCodereview{codereviewarn: repo.CodeReviewArn})
+        MERGE (cgc)-[r:HAS_RECOMMENDATION]->(cgr)
+        ON CREATE SET cgr.firstseen = timestamp()
+        SET cgr.lastupdated = {aws_update_tag}
+    """
+
+    neo4j_session.run(
+        cypher,
+        Repos=data,
+        Region=region,
+        AWS_ACCOUNT_ID=current_aws_account_id,
+        aws_update_tag=aws_update_tag,
+    )
+
+@timeit
+def load_recommendation_feedbacks(
+    neo4j_session: neo4j.Session,
+    data: list,
+    region: str,
+    current_aws_account_id: str,
+    aws_update_tag: int,
+) -> None:
+    cypher = """
+    UNWIND {Repos} AS repo
+        MERGE (crf:CodeguruRecommendationFeedback{recommendationid: repo.RecommendationId})
+        ON CREATE SET crf.firstseen = timestamp()
+        SET  
+             crf.reactions = repo.Reactions,
+             crf.lastupdated = {aws_update_tag}
+        WITH crf,repo
+        MATCH (cgr:CodeguruRecommendation{recommendationid: repo.RecommendationId})
+        MERGE (cgr)-[r:HAS_RECOMMENDATION_FEEDBACK]->(crf)
+        ON CREATE SET crf.firstseen = timestamp()
+        SET crf.lastupdated = {aws_update_tag}
+    """
+
+    neo4j_session.run(
+        cypher,
+        Repos=data,
+        Region=region,
+        AWS_ACCOUNT_ID=current_aws_account_id,
+        aws_update_tag=aws_update_tag,
+    )
+
+@timeit
+def cleanup_codeguru_associations(neo4j_session: neo4j.Session, common_job_parameters: Dict) -> None:
+    run_cleanup_job('aws_import_codeguru_associations_cleanup.json', neo4j_session, common_job_parameters)
+
+@timeit
+def cleanup_codeguru_codereviews(neo4j_session: neo4j.Session, common_job_parameters: Dict) -> None:
+    run_cleanup_job('aws_import_codeguru_codereviews_cleanup.json', neo4j_session, common_job_parameters)
 
 @timeit
 def sync(
     neo4j_session: neo4j.Session, boto3_session: boto3.session.Session, regions: List[str], current_aws_account_id: str,
     update_tag: int, common_job_parameters: Dict,
 ) -> None:
+    regions = boto3_session.get_available_regions('codeguru-reviewer')
+
     for region in regions:
-        if region == 'ap-northeast-1':
-            logger.info("Syncing CodeGuru for region '%s' in account '%s'.", region, current_aws_account_id)
-            associations = get_repository_associations(boto3_session, region)
-            if len(associations) == 0:
-                continue
-            logger.info(associations)
-            association_name = get_association_name(associations)
-            code_reviews = get_code_reviews(boto3_session, region, association_name)
+        logger.info("Syncing CodeGuru for region '%s' in account '%s'.", region, current_aws_account_id)
+        associations = get_repository_associations(boto3_session, region)
+        if len(associations) == 0:
+            continue
+        logger.info(associations)
+        association_names = get_association_names(associations)
+
+        load_repository_associations(neo4j_session, associations, region, current_aws_account_id, update_tag)
+
+        code_reviews = get_code_reviews(boto3_session, region, association_names)
+        load_code_reviews(neo4j_session, code_reviews, region, current_aws_account_id, update_tag)
+
+        recommendations = get_recommendations(boto3_session, region, code_reviews)
+        load_recommendations(neo4j_session, recommendations, region, current_aws_account_id, update_tag)
+
+        codereview_arns=get_recommendation_feedbacks(boto3_session, region, code_reviews)
+        load_recommendation_feedbacks(neo4j_session, codereview_arns, region, current_aws_account_id, update_tag)
+
+        cleanup_codeguru_codereviews(neo4j_session, common_job_parameters)
+        cleanup_codeguru_associations(neo4j_session, common_job_parameters)
